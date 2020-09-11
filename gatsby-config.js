b34f53152a41f2161484610ba513a76c1a2e998b
anvaypandey <54306681+anvaypandey@users.noreply.github.com>
2020-09-11 14:08:04 +1000

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,29 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+ const fetch = require('isomorphic-fetch')
+ const {createHttpLink} = require ("apollo-link-http")
+
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-graphql',
+
+      options: {
+         typeName : 'hasura',
+         fieldName : 'test',
+         createLink: pluginOptions => {
+          return createHttpLink({
+            uri: 'https://fresh-jackal-96.hasura.app/v1/graphql',
+            headers: {
+              'x-hasura-admin-secret': 'TzFXJm86RxahHsg',
+            },
+            fetch,
+          })
+        }
+      }
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,27 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import { graphql } from 'gatsby';
+
+const naam = "Anvay";
+
+export const GET_DOGS = (graphql`
+query MyQuery ($name : String){
+  test {
+    user(where: {first_name: {_eq: $name}}) {
+      user_id
+      first_name
+      last_name
+      acct_active
+    }
+  }
 }
+`,
+{
+  name : naam
+});
+
+export default ({data}) => {
+return <div>{data.test.user[0].user_id}</div>
+}
+
+
